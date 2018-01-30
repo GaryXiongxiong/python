1a40db1dc4c2eec1575d290ddf171ed4f79e1a30
亦雄 蒋 <qq279@MIIX-GARY>
2018-01-30 17:41:05 +0800

    订正了ex10.py中原作者部分笔记疏漏。补充了一些常用相关资料

--- ex10.py
+++ ex10.py
@@ -11,7 +11,24 @@ tabby_cat = "\tI'm tabbed in."
 persian_cat = "I'm split\non a line."
 backslash_cat = "I'm \\ a \\ cat."
 
-# \t是输出八个空格
+# \t是输出八个空格（nope, \t为水平制表符，可纵向对齐）
+# 转义字符	描述
+# \(在行尾时)	续行符
+# \\	反斜杠符号
+# \'	单引号
+# \"	双引号
+# \a	响铃
+# \b	退格(Backspace)
+# \e	转义
+# \000	空
+# \n	换行
+# \v	纵向制表符
+# \t	横向制表符
+# \r	回车
+# \f	换页
+# \oyy	八进制数yy代表的字符，例如：\o12代表换行
+# \xyy	十进制数yy代表的字符，例如：\x0a代表换行
+# \other	其它的字符以普通格式输出
 # \n是换行
 fat_cat = """
 I'll do a list:
